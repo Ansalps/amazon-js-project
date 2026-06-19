bc2d2386819a0ebd04a63be5c00ee0b1f259374c
Buddizansal <buddizansal@gmail.com>
2026-06-19 17:40:36 +0530

    Create automated tests for orderSummary

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -5,6 +5,6 @@ export function renderCheckoutHeader(){
      checkoutHeaderHTML+=`
         Checkout (<a class="return-to-home-link js-return-to-home-link"
             href="amazon.html">${cartQuantity} Items</a>)
-     `
+     `;
      document.querySelector('.js-checkout').innerHTML=checkoutHeaderHTML;
 }
